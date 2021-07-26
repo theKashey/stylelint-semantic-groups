fb217405939b0c3273814f94c5e6ca53f1e52d8b
Anton Korzunov <thekashey@gmail.com>
2021-07-26 19:47:18 +1000

    fix: add composite properties place-content, place-items, place-set and inset

--- src/groups/element-properties.ts
+++ src/groups/element-properties.ts
@@ -1,6 +1,6 @@
 export default [
   ['grid-area', 'grid-row', 'grid-column', 'grid-column-start', 'grid-column-end', 'grid-row-start', 'grid-row-end'],
-  ['position', 'top', 'left', 'bottom', 'right', 'content', 'z-index'],
+  ['position', 'inset', 'top', 'left', 'bottom', 'right', 'content', 'z-index'],
   // moved to box
   // ['width', 'height', 'margin', 'margin-top', 'margin-right', 'margin-bottom', 'margin-left'],
 
